388f42f47ad2f9b2260f61139f713805d87ffd44
Matt Sievers <msievers@thegeneral.com>
2019-01-30 18:30:28 -0600

    Week 7
    Format of driver for printing updated to all be on one page

--- src/Final_Project/Milestone3Driver.java
+++ src/Final_Project/Milestone3Driver.java
@@ -9,13 +9,13 @@ public class Milestone3Driver {
     NFL_Player_Manager playerManager = new NFL_Player_Manager();
     playerManager.createOffensivePlayers(6);
     System.out.println("Offensive Players are: ");
-    System.out.printf("%-37s%-15s%-15s%-26s%-15s%-15s%-15s%-15s%-15s%-15s\n", "Player Key", "First Name", "Last Name","Full Name", "Position", "Team Name",
-            "Rush", "Pass", "Receive", "SB Wins");
+    System.out.printf("%-37s%-15s%-15s%-26s%-15s%-15s%-15s%-15s%-15s%-15s\n", "Player Key", "First Name", "Last Name",
+            "Full Name", "Position", "Team Name", "Rush", "Pass", "Receive", "SB Wins");
     System.out.println(playerManager.toString());
     playerManager.createDefensivePlayers(3);
     System.out.println("Defensive Players are: " + "\n");
-    System.out.printf("%-37s%-15s%-15s%-26s%-15s%-15s%-15s%-15s%-15s%-15s\n", "Player Key", "First Name", "Last Name","Full Name", "Position", "Team Name",
-            "D Tackles", "D Sacks", "D Int", "SB Wins");
+    System.out.printf("%-37s%-15s%-15s%-26s%-15s%-15s%-15s%-15s%-15s%-15s\n", "Player Key", "First Name", "Last Name",
+            "Full Name", "Position", "Team Name", "D Tackles", "D Sacks", "D Int", "SB Wins");
     System.out.println(playerManager.toString());
 
 
